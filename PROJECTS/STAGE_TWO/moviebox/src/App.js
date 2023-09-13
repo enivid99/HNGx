e3b25e2573a3a6c672cd4d71578ca7f95657e195
kreatedbyenivid <divindavid6677@gmail.com>
2023-09-13 14:24:19 +0100

    Version History
    Started with Routing

--- PROJECTS/STAGE_TWO/moviebox/src/App.js
+++ PROJECTS/STAGE_TWO/moviebox/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import Home from './pages/Home'
+import Movie from './pages/Movie'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+          <Routes>
+               <Route path='/' element={<Home/>} />
+               <Route path='/movie/:movieId
+               ' element={<Movie/>} />
+          </Routes>
+      </Router>
+    </>
   );
 }
 
